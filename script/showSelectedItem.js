c81123a4ecd112f6d9675e14d77021cb24780408
Moumen <ma1910156@qu.edu.qa>
2024-03-24 12:27:38 +0300

    Just Saving

--- script/showSelectedItem.js
+++ script/showSelectedItem.js
@@ -1,22 +1,33 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
 
     const searchBar = document.querySelector("#search-form");
+    let desiredBookName = "";
 
-    searchBar.addEventListener("change", () => {
-        const booksAvailable = document.querySelectorAll("li");
+    if (searchBar) {
+        searchBar.addEventListener("change", () => {
+            const booksAvailable = document.querySelectorAll("li");
 
-        booksAvailable.forEach(book => {
-            console.log(book);
+            booksAvailable.forEach(book => {
+                if (book.textContent === searchBar.value) {
+                    desiredBookName = book.textContent;
+                    window.location.href = "/toBuy.html";
+                }
+            });
         });
-    });
+    }
+
+    // const booksFile = fetch("\Phase 1 (M.Reyad)\json\items.json");
+    // console.log(JSON.parse(booksFile));
+    // console.log(booksFile);
+
+    const response = await fetch("/Phase 1 (M.Reyad)/json/items.json");
+    const books = await response.json();
+
+    console.log(books[0].title);
+
+    const desiredBook = books.find((book, index) => book.title === desiredBookName);
+    console.log(desiredBook);
+    console.log(5345);
+
 
 });
-
-
-
-
-
-// booksAvailable.addEventListener("change", () => {
-//     console.log(booksAvailable);
-// });
-
